9628d5aa975b30a1cc964f27a6034f36241dde87
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2024-08-25 16:32:17 -0300

    Finalizei um placeholder para o menu e já configurei a configuração da música e sfx

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,15 @@
 extends Control
 
+@onready var music_player: AudioStreamPlayer = $"Music Player"
+@onready var button_audio: AudioStreamPlayer = $Button_audio_test/button_audio
+
+@onready var MASTER_BUS_ID = AudioServer.get_bus_index("Master")
+@onready var MUSIC_BUS_ID = AudioServer.get_bus_index("Music")
+@onready var SFX_BUS_ID = AudioServer.get_bus_index("SFX")
+
+@onready var audio_menu: Control = %"Audio Menu"
+@onready var buttons: Control = %"Main Buttons"
+
 func _on_button_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 	
@@ -7,10 +17,28 @@ func _on_button_load_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/load_menu.tscn")
 	
 func _on_button_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+	buttons.visible = false
+	audio_menu.visible = true
+
+func _on_comeback_button_pressed() -> void:
+	buttons.visible = true
+	audio_menu.visible = false
 
 func _on_button_extras_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/extras_menu.tscn")
 
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_button_audio_test_pressed() -> void:
+	button_audio.play()
+
+# Audios
+func _on_master_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(MASTER_BUS_ID, linear_to_db(value))
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(MUSIC_BUS_ID, linear_to_db(value))
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(SFX_BUS_ID, linear_to_db(value))
